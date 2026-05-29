c871cd482d0cd021954b13274144fce37ce54320
Larysa <lorasperling@gmail.com>
2026-05-29 11:12:27 +0200

    Add User model

---
+++ src/models/User.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      trim: true,
+      minlength: 3,
+      maxlength: 30
+    },
+
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"]
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user"
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+export default mongoose.model("User", userSchema);
