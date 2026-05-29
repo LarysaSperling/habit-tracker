6a584bf3ef75e5e9479f31c571cf52da712d65fa
Larysa <lorasperling@gmail.com>
2026-05-29 11:19:24 +0200

    Add API route definitions

--- src/routes/habitRoutes.js
+++ src/routes/habitRoutes.js
@@ -1,5 +1,20 @@
 import express from "express";
+import {
+  createHabit,
+  getHabits,
+  getHabitById,
+  updateHabit,
+  deleteHabit,
+  completeHabit
+} from "../controllers/habitController.js";
 
 const router = express.Router();
+
+router.post("/", createHabit);
+router.get("/", getHabits);
+router.get("/:id", getHabitById);
+router.put("/:id", updateHabit);
+router.delete("/:id", deleteHabit);
+router.post("/:id/complete", completeHabit);
 
 export default router;
