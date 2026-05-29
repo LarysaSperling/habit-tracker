07282444da436a698216f56cd1c8a1e9ad96bc2f
Larysa <lorasperling@gmail.com>
2026-05-29 11:20:49 +0200

    Add auth routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,16 @@
 import express from "express";
+import {
+  register,
+  login,
+  getAuthUser
+} from "../controllers/authController.js";
+
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/", authMiddleware, getAuthUser);
 
 export default router;
